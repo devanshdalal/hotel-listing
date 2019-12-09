7cc9149ef721d8680bca9034585050ae6b3f7f43
manish-dalal <md4640@gmail.com>
2019-12-10 01:39:44 +0530

    Updates

---
+++ precache-manifest.d6503a4d3b07235251a93debbbb16cdc.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "834119a4f950325ef0a60a62714a3325",
+    "url": "/hotel-listing/index.html"
+  },
+  {
+    "revision": "ed2521f3c99833139404",
+    "url": "/hotel-listing/static/css/2.912cdde5.chunk.css"
+  },
+  {
+    "revision": "97631afcd74b064364c0",
+    "url": "/hotel-listing/static/css/main.d59dad2c.chunk.css"
+  },
+  {
+    "revision": "ed2521f3c99833139404",
+    "url": "/hotel-listing/static/js/2.abe5a94c.chunk.js"
+  },
+  {
+    "revision": "2680491fb1fba8f9fc72d64c5b2f7c55",
+    "url": "/hotel-listing/static/js/2.abe5a94c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "97631afcd74b064364c0",
+    "url": "/hotel-listing/static/js/main.45596f6b.chunk.js"
+  },
+  {
+    "revision": "445646580e466c3a33dd",
+    "url": "/hotel-listing/static/js/runtime-main.ba34b2c6.js"
+  },
+  {
+    "revision": "5f4f2e9d4d9824b6694dd120e23d42f6",
+    "url": "/hotel-listing/static/media/defaultBg.5f4f2e9d.jpg"
+  },
+  {
+    "revision": "73780b736a4ab91880297d574e4c63c6",
+    "url": "/hotel-listing/static/media/logo.73780b73.png"
+  }
+]);
